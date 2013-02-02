d5c5209ad139bf9fc3141d80997e2936f3a69d87
Fernando Guillen <fguillen.mail@gmail.com>
2013-02-02 16:52:14 +0100

    First working version

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
+# Chupacabras (UtraBeta)
 
-# Chupacabras (UtralBeta)
+(Firefox OS experimental application)
 
-* http://www.techrepublic.com/blog/webmaster/how-to-work-with-rss-using-google-feed-api-and-javascript/1900
+Very simple example of a RSS reader.
+
+* You add an URL
+* The RSS is downloaded
+* You can read it offline
+
+## Using
+
+* [App template](https://github.com/mozilla/mortar-tab-view)
+* [Google RSS API](http://www.techrepublic.com/blog/webmaster/how-to-work-with-rss-using-google-feed-api-and-javascript/1900)
+
+## TODO
+
+* Checking for no connection so not external JS are loaded an not 'ADD RSS' button
+* Design!!!
---
+++ js/app.js
@@ -0,0 +1,64 @@
+console.log("here6");
+
+// This uses require.js to structure javascript:
+// http://requirejs.org/docs/api.html#define
+
+define(function(require) {
+    // Receipt verification (https://github.com/mozilla/receiptverifier)
+    require('receiptverifier');
+
+    // Installation button
+    require('./install-button');
+
+    // Install the layouts
+    require('layouts/layouts');
+
+    // Write your app here.
+
+    function formatDate(d) {
+        return (d.getMonth()+1) + '/' +
+            d.getDate() + '/' +
+            d.getFullYear();
+     }
+
+
+
+    // Passing a function into $ delays the execution until the
+    // document is ready
+    $(function() {
+
+        console.log("here2");
+
+        // List view
+        var list = $('.list').get(0);
+        // list.add({ title: 'Cook yummy food',
+        //            desc: 'COOK ALL THE THINGS',
+        //            date: new Date() });
+        // list.add({ title: 'Make things',
+        //            desc: 'Make this look like that',
+        //            date: new Date(12, 9, 5) });
+        // list.add({ title: 'Move stuff',
+        //            desc: 'Move this over there',
+        //            date: new Date(12, 10, 1) });
+        // list.add({ title: 'Cook yummy food',
+        //            desc: 'COOK ALL THE THINGS',
+        //            date: new Date() });
+        // list.add({ title: 'Make things',
+        //            desc: 'Make this look like that',
+        //            date: new Date(12, 9, 5) });
+
+        list.nextView = 'x-view.details';
+
+        // Detail view
+        var details = $('.details').get(0);
+        details.render = function(item) {
+            $('.rss_title', this).text(item.get('title'));
+            $('.rss_content', this).html(item.get('desc'));
+
+            console.log("render", item.get("title"));
+            console.log("desc", item.get("desc"));
+        };
+
+        start();
+    });
+});
